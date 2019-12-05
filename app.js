bd816f49ba9c72aec02bd12fcc61f4c8f9920603
jorgeortiz1228 <jorgeortiz1228@gmail.com>
2019-12-05 01:42:10 -0800

    added post, user, and auth functionalit

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
+const cors = require('cors');
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+//npm install --save mongoose
+const mongoose = require('mongoose');
+//npm install --save multer
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if ( 
+        file.mimetype === 'image/png' || 
+        file.mimetype === 'image/jpg' || 
+        file.mimetype === 'image/jpeg' 
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+//cors solution ting
+app.options('*', cors());
+app.use(cors());
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,5 +51,21 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(8008); //max used 8000
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+    .connect('mongodb+srv://Jay:smallcouncil@cluster0-7xgym.mongodb.net/messages?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(8008); //max used 8000
+    })
+    .catch(err => {
+        console.log(err);
+    });
