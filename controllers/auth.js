ca8b52ddb3c4d6b5a2ea1c45235a58350dd2ea87
jorgeortiz1228 <jorgeortiz1228@gmail.com>
2019-12-05 19:10:55 -0800

    implemented authorization and status

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 //npm install --save bcryptjs
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -26,6 +27,85 @@ exports.signup = (req, res, next) => {
         })
         .then(result => {
             res.status(201).json({ message: 'User created !', userId: result._id });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                const error = new Error('No user with this email was found .')
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password)
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error('Wrong password !');
+                error.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign(
+                {
+                    email: loadedUser.email,
+                    userId: loadedUser._id.toString()
+                },
+                'somesupersecretsecret',
+                { expiresIn: '1h' }    
+            );
+            res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.getUserStatus = (req, res, next) => {
+    User.findById(req.userId)
+        .then(user => {
+            if (!user) {
+                const error = new Error('No user found .');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({ status: user.status });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.updateUserStatus = (req, res, next) => {
+    const newStatus = req.body.status;
+    User.findById(req.userId)
+        .then(user => {
+            if (!user) {
+                const error = new Error('No user found .');
+                error.statusCode = 404;
+                throw error;
+            }
+            user.status = newStatus;
+            return user.save();
+        })
+        .then(result => {
+            res.status(200).json({ message: 'User status updated .' });
         })
         .catch(err => {
             if (!err.statusCode) {
--- controllers/feed.js
+++ controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -47,20 +48,28 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
+    let creator;
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: {
-            name: 'Jay'
-        }
+        creator: req.userId
     });
     post.save()
+        .then(result => {
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
         .then(result => {
             console.log(result);
             res.status(201).json({
                 message: 'Post createds successfully !',
-                post: result
+                post: post,
+                creator: { _id: creator._id, name: creator.name }
             });
         })
         .catch(err => {
@@ -116,6 +125,11 @@ exports.updatePost = (req, res, next) => {
                 error.statusCode = 404;
                 throw error;
             }
+            if (post.creator.toString() !== req.userId) {
+                const error = new Error('Not authorized to edit !');
+                error.statusCode = 403;
+                throw error;
+            }
             if (imageUrl !== post.imageUrl) {
                 clearImage(post.imageUrl);
             }
@@ -144,9 +158,21 @@ exports.deletePost = (req, res, next) => {
                 error.statusCode = 404;
                 throw error;
             }
+            if (post.creator.toString() !== req.userId) {
+                const error = new Error('Not authorized to edit !');
+                error.statusCode = 403;
+                throw error;
+            }
             //check logged in user
             clearImage(post.imageUrl);
             return Post.findByIdAndRemove(postId);
+        })
+        .then(result => {
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            user.posts.pull(postId);
+            return user.save();
         })
         .then(result => {
             console.log(result);
